5a669c1e235997546056390f3c530028b1a29e52
Mong Corleone <ibamong@hotmail.com>
2017-06-03 22:21:48 +0200

    Update Constants.cs

--- mori9/Utils/Constants.cs
+++ mori9/Utils/Constants.cs
@@ -9,14 +9,8 @@ namespace mori9.Utils
 {
 	public class Constants
 	{
-		//public const string FB_APP_ID = "";
-		//public const string FB_APP_NAME = "My Inventory";
-		//public const string PROVIDER_NAME = "graph.facebook.com";
-
-		public const string COGNITO_IDENTITY_POOL_ID = "us-east-1:976b965c-56ad-4680-9e46-8edef60b095c";
-		public static RegionEndpoint COGNITO_REGION = RegionEndpoint.USEast1; // N. Virginia
-
-		public static RegionEndpoint DYNAMODB_REGION = RegionEndpoint.APSoutheast1; // Singapore
-
+		public const string COGNITO_IDENTITY_POOL_ID = "xxxxxxx";  //  Enter your ID
+		public static RegionEndpoint COGNITO_REGION = RegionEndpoint.USEast1; // Enter your region
+		public static RegionEndpoint DYNAMODB_REGION = RegionEndpoint.APSoutheast1; // Enter your region
 	}
 }
